f7ccf37454c818d6108752f4149b266ff09393bd
VadimKorotaev <vadim231101@mail.ru>
2021-09-07 00:07:15 +0500

    improved lifting and lowering of the tool between contours

--- Program/generate_program.py
+++ Program/generate_program.py
@@ -1,29 +1,50 @@
 
+def create_axis_x(point):
+	axis_x = '''+%s'''%(int(point / 2 + 220)) if int(point / 2 + 220) > 0 else '''%s'''%(int(point / 2 + 220))
+	return axis_x
+
+def create_axis_y(point):
+	axis_y = '''+%s'''%(int(point / 2 - 50)) if int(point / 2 - 50) > 0 else '''%s'''%(int(point / 2 - 50))
+	return axis_y
+
 def create_program(contours): 
-	print(contours)
 	axis_z_up = '190.00'
 	axis_z_down = '184.86'
 	program = ''
 	number_line = 0
-	number_contour = 0
 	for contour in contours:
-		i = 0
+		number_point = 0
 		for point in contour:
-			i += 1
-			if i == 1:
+			number_point += 1
+			if number_point == 1:
 				number_line += 1
+				axis_x = create_axis_x(point[0][1])
+				axis_y = create_axis_y(point[0][0])
 				axis_z = axis_z_up
-				point_x = '''+%s'''%(int(point[0][1] / 2 + 220)) if int(point[0][1] / 2 + 220) > 0 else '''%s'''%(int(point[0][1] / 2 + 220))
-				point_y = '''+%s'''%(int(point[0][0] / 2 - 50)) if int(point[0][0] / 2 - 50) > 0 else '''%s'''%(int(point[0][0] / 2 - 50))
-				program += ('''%s p1 = (%s.00,%s.00,+%s)\n'''%(number_line, point_x, point_y, axis_z))
+				program += ('''%s p1 = (%s.00,%s.00,+%s)\n'''%(number_line, axis_x, axis_y, axis_z))
+				number_line += 1
+				program += ('''%s Mov p1\n'''%(number_line))
+			elif number_point == 2:
+				number_line += 1
+				axis_x = create_axis_x(point[0][1])
+				axis_y = create_axis_y(point[0][0])
+				axis_z = axis_z_up
+				program += ('''%s p1 = (%s.00,%s.00,+%s)\n'''%(number_line, axis_x, axis_y, axis_z))
+				number_line += 1
+				program += ('''%s Mov p1\n'''%(number_line))
+				number_line += 1
+				axis_x = create_axis_x(point[0][1])
+				axis_y = create_axis_y(point[0][0])
+				axis_z = axis_z_down
+				program += ('''%s p1 = (%s.00,%s.00,+%s)\n'''%(number_line, axis_x, axis_y, axis_z))
 				number_line += 1
 				program += ('''%s Mov p1\n'''%(number_line))
 			else:
-				number_line += 1
-				axis_z = axis_z_down  
-				point_x = '''+%s'''%(int(point[0][1] / 2 + 220)) if int(point[0][1] / 2 + 220) > 0 else '''%s'''%(int(point[0][1] / 2 + 220))
-				point_y = '''+%s'''%(int(point[0][0] / 2 - 50)) if int(point[0][0] / 2 - 50) > 0 else '''%s'''%(int(point[0][0] / 2 - 50))
-				program += ('''%s p1 = (%s.00,%s.00,+%s)\n'''%(number_line, point_x, point_y, axis_z))
+				number_line += 1 
+				axis_x = create_axis_x(point[0][1])
+				axis_y = create_axis_y(point[0][0])
+				axis_z = axis_z_down 
+				program += ('''%s p1 = (%s.00,%s.00,+%s)\n'''%(number_line, axis_x, axis_y, axis_z))
 				number_line += 1
 				program += ('''%s Mov p1\n'''%(number_line))
 	return program
